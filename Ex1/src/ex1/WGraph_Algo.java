385fab44a3c96c98bcb3f648e1bfdf30f9f4e30b
Rotem Halbreich <66558110+RotemHalbreich@users.noreply.github.com>
2020-11-12 02:51:43 +0200

    commint 3

--- Ex1/src/ex1/WGraph_Algo.java
+++ Ex1/src/ex1/WGraph_Algo.java
@@ -2,43 +2,104 @@ package ex1;
 
 import java.util.List;
 
+/**
+ * This interface represents an Undirected (positive) Weighted Graph Theory algorithms including:
+ * 0. clone(); (copy)
+ * 1. init(graph);
+ * 2. isConnected();
+ * 3. double shortestPathDist(int src, int dest);
+ * 4. List<node_data> shortestPath(int src, int dest);
+ * 5. Save(file);
+ * 6. Load(file);
+ *
+ * @author Rotem Halbreich
+ *
+ */
+
 public class WGraph_Algo implements weighted_graph_algorithms {
 
+    /**
+     * Init the graph on which this set of algorithms operates on.
+     * @param g
+     */
     @Override
     public void init(weighted_graph g) {
 
     }
 
+    /**
+     * Return the underlying graph of which this class works.
+     * @return
+     */
     @Override
     public weighted_graph getGraph() {
         return null;
     }
 
+    /**
+     * Compute a deep copy of this weighted graph.
+     * @return
+     */
     @Override
     public weighted_graph copy() {
         return null;
     }
 
+    /**
+     * Returns true if and only if (iff) there is a valid path from EVREY node to each
+     * other node. NOTE: assume ubdirectional graph.
+     * @return
+     */
     @Override
     public boolean isConnected() {
         return false;
     }
 
+    /**
+     * returns the length of the shortest path between src to dest
+     * Note: if no such path --> returns -1
+     * @param src - start node
+     * @param dest - end (target) node
+     * @return
+     */
     @Override
     public double shortestPathDist(int src, int dest) {
         return 0;
     }
 
+    /**
+     * returns the the shortest path between src to dest - as an ordered List of nodes:
+     * src--> n1-->n2-->...dest
+     * see: https://en.wikipedia.org/wiki/Shortest_path_problem
+     * Note if no such path --> returns null;
+     * @param src - start node
+     * @param dest - end (target) node
+     * @return
+     */
     @Override
     public List<node_info> shortestPath(int src, int dest) {
         return null;
     }
 
+    /**
+     * Saves this weighted (undirected) graph to the given
+     * file name
+     * @param file - the file name (may include a relative path).
+     * @return true - iff the file was successfully saved
+     */
     @Override
     public boolean save(String file) {
         return false;
     }
 
+    /**
+     * This method load a graph to this graph algorithm.
+     * if the file was successfully loaded - the underlying graph
+     * of this class will be changed (to the loaded one), in case the
+     * graph was not loaded the original graph should remain "as is".
+     * @param file - file name
+     * @return true - iff the graph was successfully loaded.
+     */
     @Override
     public boolean load(String file) {
         return false;
--- Ex1/src/ex1/WGraph_DS.java
+++ Ex1/src/ex1/WGraph_DS.java
@@ -3,6 +3,15 @@ package ex1;
 import java.util.Collection;
 import java.util.HashMap;
 
+/**
+ * This class represents an undirectional weighted graph.
+ * It should support a large number of nodes (over 10^6, with average degree of 10).
+ * The implementation should be based on an efficient compact representation
+ * (should NOT be based on a n*n matrix).
+ *
+ * @author Rotem Halbreich
+ */
+
 public class WGraph_DS implements weighted_graph {
 
     private int v_size = 0;
@@ -11,12 +20,17 @@ public class WGraph_DS implements weighted_graph {
     private HashMap<Integer, node_info> vertices;
     private HashMap<Integer, HashMap<Integer, Double>> edges;
 
-    // Constructor:
+    // Default constructor:
     public WGraph_DS() {
         this.vertices = new HashMap<Integer, node_info>();
         this.edges = new HashMap<Integer, HashMap<Integer, Double>>();
 
     }
+
+    /**
+     * This sub class represents the info of the graph's vertices
+     *
+     */
 
     private class NodeInfo implements node_info {
 
@@ -25,50 +39,86 @@ public class WGraph_DS implements weighted_graph {
         private double tag;
         private int count;
 
+        // Default constructor:
         public NodeInfo() {
             this.key = count++;
             this.info = "";
             this.tag = 0;
         }
 
+        // Constructor:
         public NodeInfo(int id) {
             this.key = id;
             this.info = "";
             this.tag = 0;
         }
 
-
+        /**
+         * Return the key (id) associated with this node.
+         * Note: each node_data should have a unique key.
+         * @return
+         */
         @Override
         public int getKey() {
             return key;
         }
 
+        /**
+         * return the remark (meta data) associated with this node.
+         * @return
+         */
         @Override
         public String getInfo() {
             return info;
         }
 
+        /**
+         * Allows changing the remark (meta data) associated with this node.
+         * @param s
+         */
         @Override
         public void setInfo(String s) {
             info = s;
         }
 
+        /**
+         * Temporal data (aka distance, color, or state)
+         * which can be used be algorithms
+         * @return
+         */
         @Override
         public double getTag() {
             return tag;
         }
 
+        /**
+         * Allow setting the "tag" value for temporal marking an node - common
+         * practice for marking by algorithms.
+         * @param t - the new value of the tag
+         */
         @Override
         public void setTag(double t) {
             tag = t;
         }
     }
 
+    /**
+     * return the node_data by the node_id,
+     * @param key - the node_id
+     * @return the node_data by the node_id, null if none.
+     */
     @Override
     public node_info getNode(int key) {
         return vertices.get(key);
     }
 
+    /**
+     * return true iff (if and only if) there is an edge between node1 and node2
+     * Note: this method should run in O(1) time.
+     * @param node1
+     * @param node2
+     * @return
+     */
     @Override
     public boolean hasEdge(int node1, int node2) {
         // if(node1 == node2) return false;
@@ -79,12 +129,26 @@ public class WGraph_DS implements weighted_graph {
         return false;
     }
 
+    /**
+     * return the weight if the edge (node1, node1). In case
+     * there is no such edge - should return -1
+     * Note: this method should run in O(1) time.
+     * @param node1
+     * @param node2
+     * @return
+     */
     @Override
     public double getEdge(int node1, int node2) {
         if (!hasEdge(node1, node2)) return -1;
         return edges.get(node1).get(node2).doubleValue();
     }
 
+    /**
+     * add a new node to the graph with the given key.
+     * Note: this method should run in O(1) time.
+     * Note2: if there is already a node with such a key -> no action should be performed.
+     * @param key
+     */
     @Override
     public void addNode(int key) {
         if (vertices.get(key) == null) {
@@ -92,49 +156,95 @@ public class WGraph_DS implements weighted_graph {
         }
     }
 
+    /**
+     * Connect an edge between node1 and node2, with an edge with weight >=0.
+     * Note: this method should run in O(1) time.
+     * Note2: if the edge node1-node2 already exists - the method simply updates the weight of the edge.
+     */
     @Override
     public void connect(int node1, int node2, double w) {
         if (w < 0) return;
 
-        if(!hasEdge(node1, node2)) {
-            HashMap<Integer, Double> dick=new HashMap<Integer, Double>();
-            dick.put(node2,w);
-            edges.put(node1,(dick));
+        if (!hasEdge(node1, node2)) {
+            HashMap<Integer, Double> w_graph = new HashMap<Integer, Double>();
+            w_graph.put(node2, w);
+            edges.put(node1, (w_graph));
         }
     }
 
+    /**
+     * This method return a pointer (shallow copy) for a
+     * Collection representing all the nodes in the graph.
+     * Note: this method should run in O(1) tim
+     * @return Collection<node_data>
+     */
     @Override
     public Collection<node_info> getV() {
+        return vertices.values();
+    }
+
+    /**
+     *
+     * This method returns a Collection containing all the
+     * nodes connected to node_id
+     * Note: this method can run in O(k) time, k - being the degree of node_id.
+     * @return Collection<node_data>
+     */
+    @Override
+    public Collection<node_info> getV(int node_id) { //<<<<<<<<<<<<<<<<<<<<
+        if (getNode(node_id) == null) return null;
         return null;
     }
 
+    /**
+     * Delete the node (with the given ID) from the graph -
+     * and removes all edges which starts or ends at this node.
+     * This method should run in O(n), |V|=n, as all the edges should be removed.
+     * @return the data of the removed node (null if none).
+     * @param key
+     */
     @Override
-    public Collection<node_info> getV(int node_id) {
+    public node_info removeNode(int key) { //<<<<<<<<<<<<<<<<<<<<<<<<<<<
         return null;
     }
 
+    /**
+     * Delete the edge from the graph,
+     * Note: this method should run in O(1) time.
+     * @param node1
+     * @param node2
+     */
     @Override
-    public node_info removeNode(int key) {
-        return null;
+    public void removeEdge(int node1, int node2) {//<<<<<<<<<<<<<<<<<<<<<<<<<<
+
     }
 
+    /** return the number of vertices (nodes) in the graph.
+     * Note: this method should run in O(1) time.
+     * @return
+     */
     @Override
-    public void removeEdge(int node1, int node2) {
-
-    }
-
-    @Override
-    public int nodeSize() {
+    public int nodeSize() { //<<<<<<<<<<<<<<<<<<<<<<<<<<<<
         return v_size;
     }
 
+    /**
+     * return the number of edges (undirectional graph).
+     * Note: this method should run in O(1) time.
+     * @return
+     */
     @Override
-    public int edgeSize() {
+    public int edgeSize() { //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
         return e_size;
     }
 
+    /**
+     * Returns the Mode Count - for testing changes in the graph.
+     * Any change in the inner state of the graph should cause an increment in the ModeCount
+     * @return mc
+     */
     @Override
-    public int getMC() {
+    public int getMC() {//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
         return mc;
     }
 }
