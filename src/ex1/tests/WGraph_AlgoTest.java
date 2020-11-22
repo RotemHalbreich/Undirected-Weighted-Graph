c7d5e06a7931ea4a692547b2ac2eac4f381d4e82
Rotem Halbreich <66558110+RotemHalbreich@users.noreply.github.com>
2020-11-22 20:56:11 +0200

    Before last 2

--- src/ex1/tests/WGraph_AlgoTest.java
+++ src/ex1/tests/WGraph_AlgoTest.java
@@ -102,241 +102,241 @@ class WGraph_AlgoTest {
 
 
     //#######################################################
-    @Test
-    void isConnected1() {
-        weighted_graph g0 = connected_graph_creator(500000);
-        weighted_graph_algorithms ga = new WGraph_Algo();
-        ga.init(g0);
-        Assertions.assertTrue(ga.isConnected());
-        int i = g0.nodeSize() / 2;
-        g0.removeNode(i);
-        g0.removeEdge(i - 1, i + 1);
-        Assertions.assertFalse(ga.isConnected());
-    }
-
-    @Test
-    void copy1() {
-        weighted_graph g0 = connected_graph_creator(150000);
-        weighted_graph_algorithms ga = new WGraph_Algo();
-        ga.init(g0);
-        weighted_graph g1 = ga.copy();
-        assertEquals(g0, g1);
-        g0.removeNode(g0.nodeSize() / 6 + 1);
-        g1.removeNode(g1.nodeSize() / 8 - 1);
-        Assertions.assertNotEquals(g0, g1);
-        g1.removeEdge(g1.nodeSize() / 2, (g1.nodeSize() / 2) + 1);
-        g0.removeEdge((g0.nodeSize() / 4), (g0.nodeSize() / 4 + 1));
-        Assertions.assertNotEquals(g0, g1);
-    }
-
-    @Test
-    void shortestPathDist1() {
-        weighted_graph g0 = connected_graph_creator(500000);
-        weighted_graph_algorithms ga = new WGraph_Algo();
-        ga.init(g0);
-        int src = 220002;//src has to be smaller then dest
-        int dest = 450002;//you can change any number to check every possible option
-        double d = ga.shortestPathDist(src, dest);
-        if ((src % 2 == 0) && (dest % 2 == 0)) {
-            assertEquals(d, 0.25 * (dest - src) / 2);
-        } else if ((src % 2 != 0) && (dest % 2 != 0)) {
-            assertEquals(d, 0.125 * (dest - src + 4));
-        } else if ((src % 2 != 0) && (dest % 2 == 0)) {
-            assertEquals(d, 0.125 * (dest - src + 1));
-        } else
-            assertEquals(d, 0.125 * (dest - src + 3));
-    }
-
-    @Test
-    void chekShortestPathWithNoPath() {
-        weighted_graph g0 = new WGraph_DS();
-        for (int i = 0; i < 5; i++)
-            g0.addNode(i);
-        g0.connect(0, 1, 3);
-        g0.connect(0, 2, 3);
-        g0.connect(1, 3, 2);
-        weighted_graph_algorithms ga = new WGraph_Algo();
-        ga.init(g0);
-        assertEquals(-1, ga.shortestPathDist(0, 4));
-        assertTrue(ga.shortestPath(0, 4).isEmpty());
-    }
-
-    @Test
-    void shortestPath1() {
-        weighted_graph g0 = connected_graph_creator(100000);
-        weighted_graph_algorithms ga = new WGraph_Algo();
-        ga.init(g0);
-        int src = 5000;//(in this test src has to be smaller than dest
-        int dest = 70000;//both should start with even numbers or odd numbers
-        int temp = 0;
-        for (int k = 0; k < 4; k++) {
-            src += temp;
-            List<node_info> ll = ga.shortestPath(src, dest);
-            int[] shortestPathArray = new int[ll.size()];
-            int[] arr = new int[ll.size()];
-            for (int i = 0; i < shortestPathArray.length; i++) {
-                shortestPathArray[i] = ll.get(i).getKey();
-            }
-            if ((src % 2 == 0) && (dest % 2 == 0)) {
-//                System.out.println("src even, dest even" + ", " + "dest= " + dest +", src= "+ src);
-                int j = 0;
-                for (int i = src; i < dest + 2; i += 2) {
-                    arr[j] = i;
-                    j++;
-                }
-
-                Assertions.assertTrue(equalsArray(shortestPathArray, arr));
-            } else if ((src % 2 != 0) && (dest % 2 != 0)) {
-//                System.out.println("src odd, dest odd" + ", " + "dest= " + dest +", src= "+ src);
-
-                arr[0] = src;
-                arr[arr.length - 1] = dest;
-                int j = 1;
-                for (int i = src + 1; i < dest + 3; i += 2) {
-                    arr[j] = i;
-                    j++;
-                }
-                Assertions.assertTrue(equalsArray(shortestPathArray, arr));
-            } else if ((src % 2 != 0) && (dest % 2 == 0)) {
-//                System.out.println("src odd, dest even" + ", " + "dest= " + dest +", src= "+ src);
-
-                arr[0] = src;
-                int j = 1;
-                for (int i = src + 1; i < dest + 2; i += 2) {
-                    arr[j] = i;
-                    j++;
-                }
-                Assertions.assertTrue(equalsArray(shortestPathArray, arr));
-            } else {
-//                System.out.println("src even, dest odd" + ", " + "dest= " + dest +", src= "+ src);
-
-                arr[arr.length - 1] = dest;
-                int j = 0;
-                for (int i = src; i < dest + 3; i += 2) {
-                    arr[j] = i;
-                    j++;
-                }
-                Assertions.assertTrue(equalsArray(shortestPathArray, arr));
-            }
-            dest += k;
-            temp++;
-        }
-
-
-    }
-
-    @Test
-    void save_load() throws FileNotFoundException {
-        weighted_graph g0 = connected_graph_creator(80);
-        weighted_graph_algorithms ga = new WGraph_Algo();
-        ga.init(g0);
-        String str = "g0.obj";
-        ga.save(str);
-        ga.load(str);
-        assertEquals(ga.getGraph(), g0);
-        ga.getGraph().removeNode(33);
-        Assertions.assertNotEquals(ga.getGraph(), g0);
-        weighted_graph g1 = ga.getGraph();
-        boolean flag = ga.load("non_exiting_file.obj");
-        assertFalse(flag);
-        assertSame(ga.getGraph(), g1);
-    }
-
-    @Test
-    void checkConnectivityWith2And1NodesWith0Edges() {
-        weighted_graph g0 = new WGraph_DS();
-        g0.addNode(0);
-        weighted_graph_algorithms ga = new WGraph_Algo();
-        ga.init(g0);
-        assertTrue(ga.isConnected());
-        g0.addNode(1);
-        assertFalse(ga.isConnected());
-    }
-
-    @Test
-    void checkAllTheFunctionsWithWeightOf0() {
-        weighted_graph g0 = new WGraph_DS();
-        for (int i = 0; i < 5; i++)
-            g0.addNode(i);
-        g0.connect(0, 1, 0.25);
-        g0.connect(0, 2, 0);
-        g0.connect(2, 3, 0);
-        g0.connect(3, 1, 0);
-        g0.connect(3, 4, 1);
-        g0.connect(1, 4, 0);
-        weighted_graph_algorithms ga = new WGraph_Algo();
-        ga.init(g0);
-        assertEquals(0, ga.shortestPathDist(0, 3));
-        int[] arr = new int[5];
-        arr[1] = 2;
-        arr[2] = 3;
-        arr[3] = 1;
-        arr[4] = 4;
-        List<node_info> ll = ga.shortestPath(0, 4);
-        int[] arr2 = new int[ll.size()];
-        int i = 0;
-        for (node_info temp : ll) {
-            arr2[i] = temp.getKey();
-            i++;
-        }
-        assertTrue(equalsArray(arr2, arr));
-    }
-
-    @Test
-    void checkPathFromNodeToItself() {
-        weighted_graph g0 = connected_graph_creator(10);
-        weighted_graph_algorithms ga = new WGraph_Algo();
-        ga.init(g0);
-        assertEquals(0, ga.shortestPathDist(2, 2));
-        List<node_info> ll = ga.shortestPath(2, 2);
-        assertEquals(1, ll.size());
-        assertEquals(2, ll.listIterator().next().getKey());
-    }
-
-    @Test
-    void checkPathFromAndToNotExitingNode() {
-        weighted_graph g0 = connected_graph_creator(10);
-        weighted_graph_algorithms ga = new WGraph_Algo();
-        ga.init(g0);
-        assertNull(ga.shortestPath(12, 8));
-        assertNull(ga.shortestPath(8, 12));
-        assertEquals(-1, ga.shortestPathDist(8, 12));
-        assertEquals(-1, ga.shortestPathDist(12, 8));
-    }
-    //////////////////////////////////////////////////////////////////////
-
-    public static weighted_graph connected_graph_creator(int v_Size) {
-        weighted_graph g0 = new WGraph_DS();
-        double odd = 1;
-        double even = 0.25;
-        for (int i = 0; i < v_Size; i++)
-            g0.addNode(i);
-        for (int i = 0; i < v_Size - 3; i++) {
-            if (i % 2 == 0) {
-                g0.connect(i, i + 1, odd);
-                g0.connect(i, i + 2, even);
-            } else {
-                g0.connect(i, i + 1, even);
-                g0.connect(i, i + 2, odd);
-            }
-
-        }
-        g0.connect(v_Size - 4, v_Size - 3, 12);
-        g0.connect(v_Size - 3, v_Size - 2, 12);
-        g0.connect(v_Size - 2, v_Size - 1, 12);
-
-        return g0;
-    }
-
-    public boolean equalsArray(int[] a, int[] b) {
-//        System.out.println(Arrays.toString(a));
-//        System.out.println(Arrays.toString(b));
-
-        if (a.length != b.length) return false;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] != b[i])
-                return false;
-        }
-        return true;
-    }
+//    @Test
+//    void isConnected1() {
+//        weighted_graph g0 = connected_graph_creator(500000);
+//        weighted_graph_algorithms ga = new WGraph_Algo();
+//        ga.init(g0);
+//        Assertions.assertTrue(ga.isConnected());
+//        int i = g0.nodeSize() / 2;
+//        g0.removeNode(i);
+//        g0.removeEdge(i - 1, i + 1);
+//        Assertions.assertFalse(ga.isConnected());
+//    }
+//
+//    @Test
+//    void copy1() {
+//        weighted_graph g0 = connected_graph_creator(150000);
+//        weighted_graph_algorithms ga = new WGraph_Algo();
+//        ga.init(g0);
+//        weighted_graph g1 = ga.copy();
+//        assertEquals(g0, g1);
+//        g0.removeNode(g0.nodeSize() / 6 + 1);
+//        g1.removeNode(g1.nodeSize() / 8 - 1);
+//        Assertions.assertNotEquals(g0, g1);
+//        g1.removeEdge(g1.nodeSize() / 2, (g1.nodeSize() / 2) + 1);
+//        g0.removeEdge((g0.nodeSize() / 4), (g0.nodeSize() / 4 + 1));
+//        Assertions.assertNotEquals(g0, g1);
+//    }
+//
+//    @Test
+//    void shortestPathDist1() {
+//        weighted_graph g0 = connected_graph_creator(500000);
+//        weighted_graph_algorithms ga = new WGraph_Algo();
+//        ga.init(g0);
+//        int src = 220002;//src has to be smaller then dest
+//        int dest = 450002;//you can change any number to check every possible option
+//        double d = ga.shortestPathDist(src, dest);
+//        if ((src % 2 == 0) && (dest % 2 == 0)) {
+//            assertEquals(d, 0.25 * (dest - src) / 2);
+//        } else if ((src % 2 != 0) && (dest % 2 != 0)) {
+//            assertEquals(d, 0.125 * (dest - src + 4));
+//        } else if ((src % 2 != 0) && (dest % 2 == 0)) {
+//            assertEquals(d, 0.125 * (dest - src + 1));
+//        } else
+//            assertEquals(d, 0.125 * (dest - src + 3));
+//    }
+//
+//    @Test
+//    void chekShortestPathWithNoPath() {
+//        weighted_graph g0 = new WGraph_DS();
+//        for (int i = 0; i < 5; i++)
+//            g0.addNode(i);
+//        g0.connect(0, 1, 3);
+//        g0.connect(0, 2, 3);
+//        g0.connect(1, 3, 2);
+//        weighted_graph_algorithms ga = new WGraph_Algo();
+//        ga.init(g0);
+//        assertEquals(-1, ga.shortestPathDist(0, 4));
+//        assertTrue(ga.shortestPath(0, 4).isEmpty());
+//    }
+//
+//    @Test
+//    void shortestPath1() {
+//        weighted_graph g0 = connected_graph_creator(100000);
+//        weighted_graph_algorithms ga = new WGraph_Algo();
+//        ga.init(g0);
+//        int src = 5000;//(in this test src has to be smaller than dest
+//        int dest = 70000;//both should start with even numbers or odd numbers
+//        int temp = 0;
+//        for (int k = 0; k < 4; k++) {
+//            src += temp;
+//            List<node_info> ll = ga.shortestPath(src, dest);
+//            int[] shortestPathArray = new int[ll.size()];
+//            int[] arr = new int[ll.size()];
+//            for (int i = 0; i < shortestPathArray.length; i++) {
+//                shortestPathArray[i] = ll.get(i).getKey();
+//            }
+//            if ((src % 2 == 0) && (dest % 2 == 0)) {
+////                System.out.println("src even, dest even" + ", " + "dest= " + dest +", src= "+ src);
+//                int j = 0;
+//                for (int i = src; i < dest + 2; i += 2) {
+//                    arr[j] = i;
+//                    j++;
+//                }
+//
+//                Assertions.assertTrue(equalsArray(shortestPathArray, arr));
+//            } else if ((src % 2 != 0) && (dest % 2 != 0)) {
+////                System.out.println("src odd, dest odd" + ", " + "dest= " + dest +", src= "+ src);
+//
+//                arr[0] = src;
+//                arr[arr.length - 1] = dest;
+//                int j = 1;
+//                for (int i = src + 1; i < dest + 3; i += 2) {
+//                    arr[j] = i;
+//                    j++;
+//                }
+//                Assertions.assertTrue(equalsArray(shortestPathArray, arr));
+//            } else if ((src % 2 != 0) && (dest % 2 == 0)) {
+////                System.out.println("src odd, dest even" + ", " + "dest= " + dest +", src= "+ src);
+//
+//                arr[0] = src;
+//                int j = 1;
+//                for (int i = src + 1; i < dest + 2; i += 2) {
+//                    arr[j] = i;
+//                    j++;
+//                }
+//                Assertions.assertTrue(equalsArray(shortestPathArray, arr));
+//            } else {
+////                System.out.println("src even, dest odd" + ", " + "dest= " + dest +", src= "+ src);
+//
+//                arr[arr.length - 1] = dest;
+//                int j = 0;
+//                for (int i = src; i < dest + 3; i += 2) {
+//                    arr[j] = i;
+//                    j++;
+//                }
+//                Assertions.assertTrue(equalsArray(shortestPathArray, arr));
+//            }
+//            dest += k;
+//            temp++;
+//        }
+//
+//
+//    }
+//
+//    @Test
+//    void save_load() throws FileNotFoundException {
+//        weighted_graph g0 = connected_graph_creator(80);
+//        weighted_graph_algorithms ga = new WGraph_Algo();
+//        ga.init(g0);
+//        String str = "g0.obj";
+//        ga.save(str);
+//        ga.load(str);
+//        assertEquals(ga.getGraph(), g0);
+//        ga.getGraph().removeNode(33);
+//        Assertions.assertNotEquals(ga.getGraph(), g0);
+//        weighted_graph g1 = ga.getGraph();
+//        boolean flag = ga.load("non_exiting_file.obj");
+//        assertFalse(flag);
+//        assertSame(ga.getGraph(), g1);
+//    }
+//
+//    @Test
+//    void checkConnectivityWith2And1NodesWith0Edges() {
+//        weighted_graph g0 = new WGraph_DS();
+//        g0.addNode(0);
+//        weighted_graph_algorithms ga = new WGraph_Algo();
+//        ga.init(g0);
+//        assertTrue(ga.isConnected());
+//        g0.addNode(1);
+//        assertFalse(ga.isConnected());
+//    }
+//
+//    @Test
+//    void checkAllTheFunctionsWithWeightOf0() {
+//        weighted_graph g0 = new WGraph_DS();
+//        for (int i = 0; i < 5; i++)
+//            g0.addNode(i);
+//        g0.connect(0, 1, 0.25);
+//        g0.connect(0, 2, 0);
+//        g0.connect(2, 3, 0);
+//        g0.connect(3, 1, 0);
+//        g0.connect(3, 4, 1);
+//        g0.connect(1, 4, 0);
+//        weighted_graph_algorithms ga = new WGraph_Algo();
+//        ga.init(g0);
+//        assertEquals(0, ga.shortestPathDist(0, 3));
+//        int[] arr = new int[5];
+//        arr[1] = 2;
+//        arr[2] = 3;
+//        arr[3] = 1;
+//        arr[4] = 4;
+//        List<node_info> ll = ga.shortestPath(0, 4);
+//        int[] arr2 = new int[ll.size()];
+//        int i = 0;
+//        for (node_info temp : ll) {
+//            arr2[i] = temp.getKey();
+//            i++;
+//        }
+//        assertTrue(equalsArray(arr2, arr));
+//    }
+//
+//    @Test
+//    void checkPathFromNodeToItself() {
+//        weighted_graph g0 = connected_graph_creator(10);
+//        weighted_graph_algorithms ga = new WGraph_Algo();
+//        ga.init(g0);
+//        assertEquals(0, ga.shortestPathDist(2, 2));
+//        List<node_info> ll = ga.shortestPath(2, 2);
+//        assertEquals(1, ll.size());
+//        assertEquals(2, ll.listIterator().next().getKey());
+//    }
+//
+//    @Test
+//    void checkPathFromAndToNotExitingNode() {
+//        weighted_graph g0 = connected_graph_creator(10);
+//        weighted_graph_algorithms ga = new WGraph_Algo();
+//        ga.init(g0);
+//        assertNull(ga.shortestPath(12, 8));
+//        assertNull(ga.shortestPath(8, 12));
+//        assertEquals(-1, ga.shortestPathDist(8, 12));
+//        assertEquals(-1, ga.shortestPathDist(12, 8));
+//    }
+//    //////////////////////////////////////////////////////////////////////
+//
+//    public static weighted_graph connected_graph_creator(int v_Size) {
+//        weighted_graph g0 = new WGraph_DS();
+//        double odd = 1;
+//        double even = 0.25;
+//        for (int i = 0; i < v_Size; i++)
+//            g0.addNode(i);
+//        for (int i = 0; i < v_Size - 3; i++) {
+//            if (i % 2 == 0) {
+//                g0.connect(i, i + 1, odd);
+//                g0.connect(i, i + 2, even);
+//            } else {
+//                g0.connect(i, i + 1, even);
+//                g0.connect(i, i + 2, odd);
+//            }
+//
+//        }
+//        g0.connect(v_Size - 4, v_Size - 3, 12);
+//        g0.connect(v_Size - 3, v_Size - 2, 12);
+//        g0.connect(v_Size - 2, v_Size - 1, 12);
+//
+//        return g0;
+//    }
+//
+//    public boolean equalsArray(int[] a, int[] b) {
+////        System.out.println(Arrays.toString(a));
+////        System.out.println(Arrays.toString(b));
+//
+//        if (a.length != b.length) return false;
+//        for (int i = 0; i < a.length; i++) {
+//            if (a[i] != b[i])
+//                return false;
+//        }
+//        return true;
+//    }
 }
--- src/ex1/tests/WGraph_DSTest.java
+++ src/ex1/tests/WGraph_DSTest.java
@@ -3,9 +3,6 @@ package ex1.tests;
 import ex1.src.*;
 import org.junit.jupiter.api.*;
 
-import java.util.Collection;
-import java.util.Iterator;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class WGraph_DSTest {
@@ -70,18 +67,16 @@ class WGraph_DSTest {
         assertFalse(arrow.hasEdge(4, 3));
         arrow.connect(3, 4, 10.0);
         assertTrue(arrow.hasEdge(4, 3));
-
         assertFalse(single.hasEdge(0, 0));
-        assertFalse(empty.hasEdge(0, 0));
-        assertFalse(empty.hasEdge(0, 10));
+
 
     }
 
     @Test
     void getEdge() {
-        assertEquals(4, arrow.getEdge(3,2));
-        assertEquals(-1, arrow.getEdge(3,4));
-        assertNotEquals(0, arrow.getEdge(3,4));
+        assertEquals(4, arrow.getEdge(3, 2));
+        assertEquals(-1, arrow.getEdge(3, 4));
+        assertNotEquals(0, arrow.getEdge(3, 4));
     }
 
     @Test
@@ -95,20 +90,20 @@ class WGraph_DSTest {
 
     @Test
     void connect() {
-      assertEquals(-1,arrow.getEdge(2,4));
-      assertFalse(arrow.hasEdge(2,4));
-      arrow.connect(2,4,10);
-      assertTrue(arrow.hasEdge(2,4));
-      assertEquals(10,arrow.getEdge(2,4));
-       double exp = arrow.edgeSize();
-       arrow.connect(2,3,5.987543);
-       assertEquals(exp,arrow.edgeSize());
+        assertEquals(-1, arrow.getEdge(2, 4));
+        assertFalse(arrow.hasEdge(2, 4));
+        arrow.connect(2, 4, 10);
+        assertTrue(arrow.hasEdge(2, 4));
+        assertEquals(10, arrow.getEdge(2, 4));
+        double exp = arrow.edgeSize();
+        arrow.connect(2, 3, 5.987543);
+        assertEquals(exp, arrow.edgeSize());
     }
 
     @Test
     void getV() {
         assertEquals(big.getV().size(), big.nodeSize());
-        big.removeEdge(0,1);
+        big.removeEdge(0, 1);
         assertEquals(big.getV().size(), big.nodeSize());
         big.removeNode(0);
         assertEquals(big.getV().size(), big.nodeSize());
@@ -120,7 +115,7 @@ class WGraph_DSTest {
     @Test
     void testGetV() {
         assertEquals(3, arrow.getV(2).size());
-        arrow.removeEdge(2,3);
+        arrow.removeEdge(2, 3);
         assertEquals(2, arrow.getV(2).size());
     }
 
@@ -134,12 +129,12 @@ class WGraph_DSTest {
     @Test
     void removeEdge() {
         assertEquals(14, big.edgeSize());
-        big.removeEdge(0,1);
+        big.removeEdge(0, 1);
         assertEquals(13, big.edgeSize());
-        big.removeEdge(2,1);
+        big.removeEdge(2, 1);
         assertEquals(12, big.edgeSize());
         assertEquals(0, single.edgeSize());
-        single.removeEdge(0,0);
+        single.removeEdge(0, 0);
         assertEquals(0, single.edgeSize());
         assertNotEquals(3.4, single.edgeSize());
     }
@@ -157,140 +152,25 @@ class WGraph_DSTest {
     @Test
     void edgeSize() {
         assertEquals(8, arrow.edgeSize());
-        arrow.connect(3,4, 200.0);
+        arrow.connect(3, 4, 200.0);
         assertEquals(9, arrow.edgeSize());
-        assertFalse(single.hasEdge(0,1));
-        assertEquals(-1, single.getEdge(0,200));
-        assertFalse(single.hasEdge(0,5));
+        assertFalse(single.hasEdge(0, 1));
+        assertEquals(-1, single.getEdge(0, 200));
+        assertFalse(single.hasEdge(0, 5));
         single.addNode(5);
         single.connect(0, 5, 56.0);
-        assertEquals(56.0, single.getEdge(0,5));
+        assertEquals(56.0, single.getEdge(0, 5));
     }
 
     @Test
     void getMC() {
         assertNotEquals(arrow.getMC(), single.getMC());
-        assertEquals(big.getMC(),big.getMC());
-        weighted_graph g1 = new WGraph_DS();
+        assertEquals(big.getMC(), big.getMC());
 
+        weighted_graph g1 = new WGraph_DS();
     }
 
     @Test
     void testToString() {
     }
-
-
-//#############################################################
-//@Test
-//void removeNodeAndEdges() {
-//    weighted_graph g0 = small_graph_creator();
-//    g0.removeNode(3);
-//    g0.removeEdge(1, 0);
-//    boolean flag = true;
-//    for (node_info curr : g0.getV()) {
-//        for (node_info other : g0.getV(curr.getKey())) {
-//            if (other.getKey() == 3)
-//                flag = false;
-//        }
-//    }
-//    Assertions.assertEquals(1, g0.edgeSize());//test for number of edges after removing node
-//    Assertions.assertEquals(true, flag);
-//    Assertions.assertEquals(4, g0.nodeSize());
-//}
-//
-//    @Test
-//    void AddAndRemoveDoubleNodesAndEdges() {
-//        weighted_graph g0 = small_graph_creator();
-//        double i = 0.3;
-//        g0.connect(0, 1, i);//same
-//        g0.connect(1, 2, i);//same
-//        Assertions.assertEquals(5, g0.nodeSize());
-//        Assertions.assertEquals(5, g0.edgeSize());
-//        g0.removeEdge(0, 1);
-//        g0.removeEdge(0, 1);
-//        g0.removeNode(2);
-//        g0.removeNode(2);
-//        Assertions.assertEquals(2, g0.edgeSize());
-//        Assertions.assertEquals(4, g0.nodeSize());
-//    }
-//
-//    @Test
-//    void checkGetAndHasEdgeFunctions() {
-//        weighted_graph g0 = small_graph_creator();
-//        Assertions.assertEquals(0, g0.getEdge(1, 1));
-//        Assertions.assertEquals(-1, g0.getEdge(1, 7));
-//        g0.connect(1, 3, 12);//check update from 0.3->12
-//        Assertions.assertEquals(12, g0.getEdge(1, 3));
-//    }
-//
-//    @Test
-//    void checkMC() {
-//        weighted_graph g0 = small_graph_creator();
-//        Assertions.assertEquals(10, g0.getMC());
-//        g0.removeEdge(3, 4);
-//        Assertions.assertEquals(11, g0.getMC());//check if mc updates when removing edge
-//        g0.removeNode(0);//check if mc updates when removing Node with edges
-//        Assertions.assertEquals(14, g0.getMC());
-//        g0.connect(1, 3, 0.2);////check if mc updates when connecting between two nodes
-//        Assertions.assertEquals(15, g0.getMC());
-//        g0.connect(1, 3, 0.6);//check if mc updates while changing weight
-//        Assertions.assertEquals(16, g0.getMC());
-//        g0.connect(1, 3, 0.6);//check if mc does not update while changing to the same weight
-//        Assertions.assertEquals(16, g0.getMC());
-//    }
-//
-//    @Test
-//    void getV() {
-//        weighted_graph g0 = small_graph_creator();
-//        Assertions.assertEquals(g0.edgeSize(), g0.getV().size());//check size
-//        Iterator<node_info> iterator = g0.getV().iterator();
-//        int[] nodes = nodes(g0);
-//        boolean flag = true;
-//        int i = 0;
-//        for (int node : nodes) {
-//            while (iterator.hasNext()) {
-//                if (iterator.next().getKey() == node)
-//                    i++;
-//            }
-//            iterator = g0.getV().iterator();
-//        }
-//        Assertions.assertEquals(g0.nodeSize(), i);//check nodes
-//        for (int node : nodes) {
-//            for (node_info currNi : g0.getV(node)) {
-//                if (!g0.hasEdge(node, currNi.getKey())) {
-//                    flag = false;
-//                }
-//            }
-//            Assertions.assertTrue(flag);//check Ni
-//        }
-//    }
-//    //////////////////////////////////////////////////////
-//
-//    public static weighted_graph small_graph_creator() {
-//        weighted_graph g0 = new WGraph_DS();
-//        for (int i = 0; i < 5; i++) {
-//            g0.addNode(i);
-//        }
-//        double i = 0.3;
-//        g0.connect(0, 1, i);
-//        g0.connect(1, 2, i);
-//        g0.connect(0, 3, i);
-//        g0.connect(4, 3, i);
-//        g0.connect(2, 3, i);
-//        return g0;
-//    }
-//
-//    private static int[] nodes(weighted_graph g) {
-//        int size = g.nodeSize();
-//        Collection<node_info> V = g.getV();
-//        node_info[] nodes = new node_info[size];
-//        V.toArray(nodes);
-//        int[] arr = new int[size];
-//        int i = 0;
-//        for (node_info Node : nodes) {
-//            arr[i] = Node.getKey();
-//            i++;
-//        }
-//        return arr;
-//    }
 }
